8751d7c1c7f0211e8036c678a82cec59ec462947
imon.roy <38348296+unownone@users.noreply.github.com>
2022-05-04 04:54:45 +0530

    Fixed ws issue

--- app/index/chats.go
+++ app/index/chats.go
@@ -7,10 +7,15 @@ import (
 )
 
 func Chats(c *fiber.Ctx) error {
+	Env := false
+	if os.Getenv("APP_ENV") == "development" {
+		Env = true
+	}
 	return c.Render("chats",
 		fiber.Map{
 			"HOST":  os.Getenv("HHost"),
 			"PORT":  os.Getenv("PPort"),
 			"Chats": "active",
+			"Env":   Env,
 		})
 }
